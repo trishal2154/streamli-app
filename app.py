e134bf460e9aec427abb2aac52858c7a5e2ed677
trishal2154 <148067104+trishal2154@users.noreply.github.com>
2024-04-07 16:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import cv2
+from PIL import Image
 
 picture=st.camera_input("Give an Expression")
 
 if picture is not None:
-  cv2.imshow(picture)
+  picture=Image.open(picture)
+  print(picture)
